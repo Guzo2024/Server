857c3dc52b9e7cdd746d9e3c21ba61b0e4bc9dff
Guzo2024 <fernandoguarnizo@gmail.com>
2025-04-01 22:42:48 +0200

    https

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = 5000;
 
 
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://server-ersq.onrender.com', credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
